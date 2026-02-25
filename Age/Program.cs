3ae4137089189e51b2330a475392d39d06d6d773
Jubayer Hasan Akash <jubalifer55@gmail.com>
2026-02-25 11:17:38 +0600

    Starting LOL

---
+++ Age/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 18)
+                Console.WriteLine("Minor.");
+            else if (age <= 36)
+                Console.WriteLine("Adult.");
+            else
+                Console.WriteLine("Senior.");
+        }
+    }
+}
